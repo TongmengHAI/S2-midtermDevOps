5dd28a196c6ce982dfa18f9e6ad6a9225f2c1ee0
TongmengHAI <tongmeng016@gmail.com>
2024-06-14 16:33:38 +0700

    add fillable

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -1,13 +1,13 @@
 <?php
 use App\Models\Student;
-use function PHPUnit\Framework\assertTrue;
 
-test('create student', function () {
+it('can create a student', function () {
     $student = Student::create([
         'name' => 'John Doe',
         'email' => 'e20201056@itc.edu.kh',
         'phone' => '012 345 678',
         'dob' => '2003-10-01'
     ]);
-    assertTrue($student->exists());
+
+    expect($student->exists())->toBeTrue();
 });
